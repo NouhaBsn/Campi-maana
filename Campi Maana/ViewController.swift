694c27e5afae6ffd5ce5c3018efd5ec0d068f934
mac <mac@MacBook-Pro-de-mac-2.local>
2018-11-27 14:40:17 +0100

    Initial Commit

---
+++ Campi Maana/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Campi Maana
+//
+//  Created by mac on 11/27/18.
+//  Copyright © 2018 mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
